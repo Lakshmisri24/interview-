69e8263511970320d1695e5174332ded51b3e265
Ganduri Lakshmi Sri <141005925+Lakshmisri24@users.noreply.github.com>
2025-09-08 14:59:22 +0530

    Update Role.java

--- interview-tracking-system/src/main/java/com/lakshmi/interview_tracking_system/dao/Role.java
+++ interview-tracking-system/src/main/java/com/lakshmi/interview_tracking_system/dao/Role.java
@@ -1,11 +1,17 @@
 package com.lakshmi.interview_tracking_system.dao;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name="roles")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
